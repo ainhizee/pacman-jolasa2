49ad15087959e7a6ce607e13ec25d37d6b3cb60a
ainhizee <93262250+ainhizee@users.noreply.github.com>
2021-12-17 14:18:02 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,26 +1,33 @@
-input.onGesture(Gesture.LogoUp, function () {
-    pacman.change(LedSpriteProperty.Y, 1)
-})
-input.onGesture(Gesture.TiltRight, function () {
-    pacman.change(LedSpriteProperty.X, -1)
-})
-input.onGesture(Gesture.TiltLeft, function () {
-    pacman.change(LedSpriteProperty.X, 1)
-})
-input.onGesture(Gesture.LogoDown, function () {
-    pacman.change(LedSpriteProperty.Y, 1)
-})
-let pacman: game.LedSprite = null
-let janaria: game.LedSprite = null
-let mamua: game.LedSprite = null
+let mamua = game.createSprite(2, 2)
+let janaria = game.createSprite(4, 4)
+let pacman = game.createSprite(0, 0)
+basic.pause(400)
 mamua.set(LedSpriteProperty.Brightness, 40)
-janaria.set(LedSpriteProperty.Blink, 50)
-pacman.set(LedSpriteProperty.Brightness, 1000)
+mamua.set(LedSpriteProperty.Blink, 50)
+janaria.set(LedSpriteProperty.Brightness, 40)
 basic.forever(function () {
-    if (mamua.isTouching(pacman)) {
+    if (pacman.isTouching(mamua)) {
         game.gameOver()
     }
     if (pacman.isTouching(janaria)) {
         game.addScore(1)
+    }
+})
+basic.forever(function () {
+    if (input.acceleration(Dimension.X) > 200) {
+        pacman.change(LedSpriteProperty.X, 1)
+        basic.pause(200)
+    }
+    if (input.acceleration(Dimension.X) < -200) {
+        pacman.change(LedSpriteProperty.X, -1)
+        basic.pause(200)
+    }
+    if (input.acceleration(Dimension.Y) > 200) {
+        pacman.change(LedSpriteProperty.Y, 1)
+        basic.pause(200)
+    }
+    if (input.acceleration(Dimension.Y) < -200) {
+        pacman.change(LedSpriteProperty.Y, -1)
+        basic.pause(200)
     }
 })
